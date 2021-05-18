fbbbeab189beca610bb5e0cceca8113b3aef1c50
ProProgrammer2504 <73734376+ProProgrammer2504@users.noreply.github.com>
2021-05-18 21:00:03 +0530

    Added routes code_confirmed and allow_notification

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import PlanLayout from "./pages/Layouts/PlanLayout";
 import Welcome from "./pages/Welcome";
 import { BrowserRouter, Route } from "react-router-dom";
 import PhoneConfirmation from "./pages/PhoneConfirmation";
+import CodeConfirmed from "./pages/CodeConfirmed";
+import AllowNotification from "./pages/AllowNotification";
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
       <PlanLayout>
         <Route exact path="/" component={Welcome} />
         <Route exact path="/invite" component={PhoneConfirmation} />
+        <Route exact path="/code_confirmed" component={CodeConfirmed} />
+        <Route exact path="/allow_notification" component={AllowNotification} />
       </PlanLayout>
     </BrowserRouter>
   );
