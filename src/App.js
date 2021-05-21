c4a8ed6da05256607bebf1869109c55f82ed598b
ProProgrammer2504 <73734376+ProProgrammer2504@users.noreply.github.com>
2021-05-21 15:06:34 +0530

    Improved the routing and added a 2nd Layout

--- src/App.js
+++ src/App.js
@@ -1,20 +1,46 @@
 import "./App.css";
 import PlanLayout from "./pages/Layouts/PlanLayout";
 import Welcome from "./pages/Welcome";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import PhoneConfirmation from "./pages/PhoneConfirmation";
 import CodeConfirmed from "./pages/CodeConfirmed";
 import AllowNotification from "./pages/AllowNotification";
+import AppLayout from "./pages/Layouts/AppLayout";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <BrowserRouter>
-      <PlanLayout>
-        <Route exact path="/" component={Welcome} />
-        <Route exact path="/invite" component={PhoneConfirmation} />
-        <Route exact path="/code_confirmed" component={CodeConfirmed} />
-        <Route exact path="/allow_notification" component={AllowNotification} />
-      </PlanLayout>
+      <Route
+        path={[
+          "/",
+          "/invite",
+          "/get_username",
+          "/code_confirmed",
+          "/allow_notification",
+        ]}
+        exact
+      >
+        <PlanLayout>
+          <Switch>
+            <Route exact path="/" component={Welcome} />
+            <Route exact path="/invite" component={PhoneConfirmation} />
+            <Route exact path="/code_confirmed" component={CodeConfirmed} />
+            <Route
+              exact
+              path="/allow_notification"
+              component={AllowNotification}
+            />
+          </Switch>
+        </PlanLayout>
+      </Route>
+      <Route exact path={["/home"]}>
+        <AppLayout>
+          <Switch>
+            <Route exact path="/home" component={Home} />
+          </Switch>
+        </AppLayout>
+      </Route>
     </BrowserRouter>
   );
 }
