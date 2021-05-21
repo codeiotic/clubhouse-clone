e50713c529675e7296e8463ed3777060a27bed0e
ProProgrammer2504 <73734376+ProProgrammer2504@users.noreply.github.com>
2021-05-21 15:07:28 +0530

    Created the Header Component

---
+++ src/components/Header.js
@@ -0,0 +1,28 @@
+import { Link } from "react-router-dom";
+import style from "../style/Header.module.css";
+
+const Header = () => {
+  return (
+    <div className={style.Header}>
+      <Link to="/explore">
+        <img src="/images/search.png" alt="Search" />
+      </Link>
+      <div className={style.nav_items}>
+        <Link to="/friends_invite">
+          <img src="/images/invite.png" alt="Invite" />
+        </Link>
+        <Link to="/upcoming">
+          <img src="/images/calendar.png" alt="Invite" />
+        </Link>
+        <Link to="/activity">
+          <img src="/images/noti.png" alt="Invite" />
+        </Link>
+        <Link to="/profile">
+          <img src="/images/b1.png" alt="Invite" />
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
