edbb901219db2a4bff2bff823edaa96863ad6274
ProProgrammer2504 <73734376+ProProgrammer2504@users.noreply.github.com>
2021-05-22 14:06:27 +0530

    Created the StartRoom Component

---
+++ src/components/bottom_sheets/StartRoom.js
@@ -0,0 +1,68 @@
+import { useState } from "react";
+import style from "../../style/BottomSheet.module.css";
+import { FcGlobe } from "react-icons/fc";
+
+const StartRoom = ({ setSheetCreateRoom, setSheetVisible }) => {
+  const [room, setRoom] = useState("open");
+
+  return (
+    <>
+      <div className={style.switch_Line}></div>
+      <div className="text-right">
+        <button className={style.addTopicBtn}>+ Add a Topic</button>
+      </div>
+      <div className={style.selectRoom}>
+        <button
+          className={room === "open" ? style.active : ""}
+          onClick={() => setRoom("open")}
+        >
+          <div>
+            <FcGlobe />
+          </div>
+          Open
+        </button>
+        <button
+          className={room === "social" ? style.active : ""}
+          onClick={() => setRoom("social")}
+        >
+          <div>
+            <FcGlobe />
+          </div>
+          Social
+        </button>
+        <button
+          className={room === "closed" ? style.active : ""}
+          onClick={() => setRoom("closed")}
+        >
+          <div>
+            <FcGlobe />
+          </div>
+          Closed
+        </button>
+      </div>
+      <p>
+        Start a Room{" "}
+        <span>
+          {room === "closed"
+            ? "For People I choose"
+            : room === "social"
+            ? "With People I follow"
+            : "Open to Everyone"}
+        </span>
+      </p>
+      <div className="text-center">
+        <button
+          className={style.letGoBtn}
+          onClick={() => {
+            setSheetCreateRoom(true);
+            setSheetVisible(true);
+          }}
+        >
+          🎉 Let's Go!
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default StartRoom;
