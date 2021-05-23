6c2036eb28e9678455110e259b1c7cabcf2bdf46
ProProgrammer2504 <73734376+ProProgrammer2504@users.noreply.github.com>
2021-05-23 13:35:11 +0530

    Gave all elements a className

--- src/components/DailyInfoCard.js
+++ src/components/DailyInfoCard.js
@@ -6,10 +6,10 @@ const DailyInfoCard = () => {
     <div className={style.dailyCard}>
       {data.map((item, index) => (
         <div key={index}>
-          <span className="">{item.time}</span>
+          <span className={style.time}>{item.time}</span>
           <div key={index}>
-            <span>{item.title}</span>
-            <p>{item.description || ""}</p>
+            <span className={style.title}>{item.title}</span>
+            <p className={style.description}>{item.description || ""}</p>
           </div>
         </div>
       ))}
