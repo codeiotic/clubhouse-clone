b770ae8b42a45a28db715396a0ca9bc402624c4b
ProProgrammer2504 <73734376+ProProgrammer2504@users.noreply.github.com>
2021-05-25 23:02:41 +0530

    Created the full Profile Component

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,8 +1,12 @@
 import style from "../style/Profile.module.css";
 import exploreStyle from "../style/Explore.module.css";
 import { Link } from "react-router-dom";
-import { BsAt, BsUpload } from "react-icons/bs";
-import { AiOutlineSetting } from "react-icons/ai";
+import { BsAt, BsPlus, BsUpload } from "react-icons/bs";
+import {
+  AiOutlineInstagram,
+  AiOutlineSetting,
+  AiOutlineTwitter,
+} from "react-icons/ai";
 
 const Profile = () => {
   return (
@@ -31,6 +35,43 @@ const Profile = () => {
             </div>
           </div>
         </div>
+        <img
+          src="/images/user-img.jpg"
+          alt="User"
+          className={style.profile_image}
+        />
+        <h4>Noob Me!</h4>
+        <p>@noobme!</p>
+        <div className={style.follow}>
+          <p>
+            <span>0</span> Followers
+          </p>
+          <p>
+            <span>51</span> Following
+          </p>
+        </div>
+        <button>Add a Bio</button>
+        <div className="mb-4">
+          <button className="mb-0">
+            <AiOutlineTwitter /> Add Twitter
+          </button>
+          <button className="mb-0">
+            <AiOutlineInstagram /> Add Instagram
+          </button>
+        </div>
+        <div className={style.nominated}>
+          <img src="/images/user-img.jpg" alt="User" />
+          <div>
+            <p>Joined 14-July-2020</p>
+            <p>
+              Nominated by <span>Anton Alarcon</span>
+            </p>
+          </div>
+        </div>
+        <p>Member of </p>
+        <button className={style.addMemberBtn}>
+          <BsPlus />
+        </button>
       </div>
     </>
   );
